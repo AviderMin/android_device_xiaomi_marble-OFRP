521cf60f0f0db15a35d4bb36d50eb06173a43cc8
AviderMin <gms2917466108@gmail.com>
2025-10-31 19:33:00 +0800

    try to fix boot slowly

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,7 +94,6 @@ TARGET_RECOVERY_DEVICE_MODULES += \
     vendor.display.config@1.0 \
     vendor.display.config@2.0 \
     libdebuggerd_client
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
