5209a190348cd61e99e0ff541a2711b1c1ab9c49
AviderMin <gms2917466108@gmail.com>
2026-05-08 12:06:23 +0800

    Modify FOX settings root directories
    Updated root directories for FOX settings and miscellaneous files.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -46,9 +46,9 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
 	export FOX_DELETE_INITD_ADDON=1
 	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
-	export FOX_SETTINGS_ROOT_DIRECTORY="/data/recovery/Fox"
+	export FOX_SETTINGS_ROOT_DIRECTORY="/data/recovery"
 	export FOX_ALLOW_EARLY_SETTINGS_LOAD=1
-	export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/sdcard/Fox"
+	export FOX_MISCELLANEOUS_ROOT_DIRECTORY="/sdcard"
 
 	# Move the Magisk installer/uninstaller ZIP to the ramdisk
 	export FOX_MOVE_MAGISK_INSTALLER_TO_RAMDISK=1
